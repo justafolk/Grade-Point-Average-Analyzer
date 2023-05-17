5c3f6064e35c913751cae095b657064c1d3aaf4d
Avdhut <avdhut.kamble776@gmail.com>
2023-05-17 18:07:17 +0530

    Added some scripts and done with alot of work

--- ai.md
+++ ai.md
@@ -1,1 +1,10 @@
 
+| Day | 10:00-11:00 | 11:00-12:00 | 12:00-13:00 | 14:00-15:00 |
+| --- | --- | --- | --- | --- |
+| Monday | DC, BFE, TOC, MVCDE | DC, MVCDE, TOC | MVCDE, DC, TOC | DC, TOC, MVCDE |
+| Tuesday | TOC, BFE, DC, MVCDE | TOC, DC, MVCDE | DC, MVCDE, TOC | DC, TOC, MVCDE |
+| Wednesday | BFE, MVCDE, DC, TOC | TOC, DC, MVCDE | TOC, DC, MVCDE | MVCDE, TOC, DC |
+| Thursday | MVCDE, DC, BFE, TOC | DC, TOC, MVCDE | DC, MVCDE, TOC | TOC, MVCDE, DC |
+| Friday | DC, MVCDE, TOC, BFE | MVCDE, DC, TOC | MVCDE, TOC, DC | DC, MVCDE, TOC |
+| Saturday | DC, TOC, MVCDE, BFE | TOC, DC, MVCDE | MVCDE, TOC, DC | TOC, DC, MVCDE |
+| Sunday | BFE, DC, MVCDE, TOC | MVCDE, DC, TOC | TOC, DC, MVCDE | DC, MVCDE, TOC |
--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, session
 from flaskext.mysql import MySQL
+import datetime
 
 app = Flask("GPA Calculator")
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'justafolk'
 app.config['MYSQL_DATABASE_DB'] = 'gpa'
 
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
 mysql = MySQL()
 mysql.init_app(app)
 conn = mysql.connect()
+
+def greet():
+    current_time = datetime.datetime.now().time()
+    if current_time.hour < 12:
+        print("Good Morning!")
+    elif current_time.hour < 18:
+        print("Good Afternoon!")
+    else:
+        print("Good Night!")
+
 class Course:
     def __init__(self, name, credits, faculty = "NONE", course_type="BSC"):
         self.name = name
@@ -16,10 +29,51 @@ class Course:
         self.faculty = faculty
         self.course_type = course_type
 
+def check_session():
+    if 'username' not in session:
+        return False
+
+    return True
+
+def create_schedule(courses, limit):
+    courses = dict(sorted(courses.items(), key=lambda item: item[1], reverse=True))
+    print(courses)
+
+    schedule = [ [] for i in range(7) ]
+    pointer = 0
+    for i, j in courses.items():
+        k = pointer
+        while j > 0:
+            schedule[k].append(i)
+            j-=1
+            k = (k+2)%7
+        pointer += 1
+    print(schedule)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect('/login')
+
+@app.route('/loginuser', methods=['POST'])
+def loginuser():
+    username = request.form['email']
+    password = request.form['pass']
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE email=%s AND password=%s", (username, password))
+    user = cursor.fetchone()
+    if user:
+        session['username'] = username
+        return redirect(url_for('mainapp'))
+    else:
+        return render_template('login.html', error="Invalid username or password")
 
 @app.route('/')
 def mainapp():
-    return render_template('index.html', title="Home")
+    if not check_session():
+        return redirect('/login')
+    return redirect('/home')
 
 @app.route('/semester/view', methods=['GET'])
 def viewsemester():
@@ -33,6 +87,9 @@ def viewcourses():
 def createcourses():
     return render_template('createcourse.html', title="Home")
 
+@app.route('/onboarding/')
+def onboardings():
+    return render_template('onboarding.html', title="Onboarding")
 
 @app.route('/semester/create')
 def onboarding():
@@ -54,8 +111,7 @@ def registeruser():
     userbranch= request.form['userbranch']  
     useremail = request.form['useremail']  
     upass     =  request.form['pass']  
-    sql = "INSERT INTO users (name, department, MIS, email, password, login_method) VALUES ('{}', '{}', '{}', '{}', '{}', '{}')".format(username, userbranch, misno, useremail, upass, "email")
-
+    sql = "INSERT INTO users (name, department, MIS, email, password, login_method, clgname) VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}')".format(username, userbranch, misno, useremail, upass, "email", clgname)
 
     cursor = conn.cursor()
     cursor.execute(sql)
@@ -63,9 +119,6 @@ def registeruser():
     cursor.close()
 
     return redirect('/')
-
-
-
 
 @app.route('/api/v1/createcourse', methods=['POST'])
 def createcourseapi():
@@ -86,8 +139,11 @@ def course_list():
     return render_template('course_list.html', title="Course List", courses=course, coursename=coursename )
 
 
+
 @app.route('/home')
 def home():
+    if not check_session():
+        return redirect('/login')
     return render_template('home.html')
 
 if __name__ == '__main__':
